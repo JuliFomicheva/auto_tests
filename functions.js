77949660224965b587aab37ffc2537014ea4a058
jfomicheva <julia.volchkova007@gmail.com>
2025-04-07 10:02:04 +0200

    response check function

--- functions.js
+++ functions.js
@@ -1,31 +1,71 @@
-let age = 38
+// let age = 38
 
-function logUserName(surname, firstname) { 
-    let userName = surname + firstname
-    console.log(userName + " age is " + age); 
-}
+// function logUserName(surname, firstname) { 
+//     let userName = surname + firstname
+//     console.log(userName + " age is " + age); 
+// }
 
-let anotherUser = "Ivan"
-let userSurname = " Pavlov"
-logUserName(anotherUser, userSurname);
-logUserName("Julia", " Fomicheva")
+// let anotherUser = "Ivan"
+// let userSurname = " Pavlov"
+// logUserName(anotherUser, userSurname);
+// logUserName("Julia", " Fomicheva")
 
 
-function isLengthMoreThan6(text) {
-    if (text > 6) {
-        console.log(true)
-    } else {
-        console.log(false)
+// function isLengthMoreThan6(text) {
+//     if (text > 6) {
+//         console.log(true)
+//     } else {
+//         console.log(false)
+//     }
+// }
+
+// isLengthMoreThan6("someWord")
+
+// function calculateLengthMoreThan6(text) {
+//     return text.length;
+// }
+// console.log(calculateLengthMoreThan6("thatIsMyText"))
+
+// let result = calculateLengthMoreThan6("sohatIsMyText")
+
+// function logTextParameter(textData) {
+//     let textInfo = result;
+//     return textInfo
+// }
+
+// console.log(logTextParameter(result))
+
+let respPositive200 = {
+    code: 200,
+    body: {
+        text: "Some Positive message"
     }
 }
 
-isLengthMoreThan6("someWord")
-
-function calculateLengthMoreThan6(text) {
-    return text.length;
+let respRedirect300 = {
+    code: 300,
+    body: {
+        text: "Some Redirect message"
+    }
 }
 
+let isResponse200 = function(code, expectedCode) {
+    if (code === expectedCode) {
+        return true
+    } else {
+        return false
+    }
 }
 
-let lengthofWord = calculateLengthMoreThan6("someWord")
-console.log(lengthofWord)
+function getTextMessage(response, codeValidation, expectedCode) {
+    let text = response.body.text;
+    if (!codeValidation(response.code, expectedCode)) {
+        console.log(`Not a ${expectedCode}`)
+    }
+    let info = {};
+    info.text = text;
+    info.code = response.code
+    return info;   
+}
+
+console.log(getTextMessage(respRedirect300, isResponse200, 200))
